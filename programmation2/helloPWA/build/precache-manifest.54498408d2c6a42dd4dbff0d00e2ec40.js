79440964b7b1a972cf13fb7a5b4520a74a0752f9
charlesetiennev <charles-etienne.villemure@hotmail.com>
2020-08-25 19:52:08 -0400

    build push

---
+++ programmation2/helloPWA/build/precache-manifest.54498408d2c6a42dd4dbff0d00e2ec40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd135ad6271df03924a880d3b6bd62f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c91ac6d34c06964dd6e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0033c867303d54378d50",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c91ac6d34c06964dd6e",
+    "url": "/static/js/2.f335625f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f335625f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0033c867303d54378d50",
+    "url": "/static/js/main.ef64a500.chunk.js"
+  },
+  {
+    "revision": "827d0b41fe9bb38586fd",
+    "url": "/static/js/runtime-main.9506ba41.js"
+  }
+]);

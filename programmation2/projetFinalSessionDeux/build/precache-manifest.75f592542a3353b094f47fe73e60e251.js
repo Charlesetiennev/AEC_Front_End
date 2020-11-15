5567a5df696d27ea1457917adf4508664860061d
charlesetiennev <charles-etienne.villemure@hotmail.com>
2020-11-15 16:46:31 -0500

    projet final avec crudcrudAPI

---
+++ programmation2/projetFinalSessionDeux/build/precache-manifest.75f592542a3353b094f47fe73e60e251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306c540131b42482d13717cdb2bbd021",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6c854e6b526eb103a68",
+    "url": "./static/css/2.1f35d23a.chunk.css"
+  },
+  {
+    "revision": "aa590e90295557a576ca",
+    "url": "./static/css/main.714110a3.chunk.css"
+  },
+  {
+    "revision": "e6c854e6b526eb103a68",
+    "url": "./static/js/2.93d0aa4b.chunk.js"
+  },
+  {
+    "revision": "a3d5da51d8489a02931a36aa8a33f4e5",
+    "url": "./static/js/2.93d0aa4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa590e90295557a576ca",
+    "url": "./static/js/main.29148c41.chunk.js"
+  },
+  {
+    "revision": "b2957c1a8ec6c12ec131",
+    "url": "./static/js/runtime-main.2a89a997.js"
+  }
+]);
